e20b1d22cb6ce2c462f4a885c78a4ec44685acaf
capybara <sakibara.ex.9175@gmail.com>
2023-07-27 20:08:45 +0900

    Hello WorldをWelcome to RaiseTech！に変更

--- src/main/java/com/Assignment6/helloworld/HelloWorldController.java
+++ src/main/java/com/Assignment6/helloworld/HelloWorldController.java
@@ -3,13 +3,17 @@ package com.Assignment6.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 public class HelloWorldController {
 
-//    ブラウザ上で[localhost:8080/capybara]と入力→[/capybara] はパス?　@GetMapping()の中の記述と同じ　
+    //    ブラウザ上で[localhost:8080/capybara]と入力→[/capybara] はパス?　@GetMapping()の中の記述と同じ　
     @GetMapping("/capybara")
-    public String sayhello(){
-        return "Hello World";
+    public String sayhello() {
+        return "Welcome to RaiseTech!";
     }
+
 
 }
