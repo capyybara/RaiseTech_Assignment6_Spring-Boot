69493566e4cd42376a0053fd4e9cdaa69c3cf409
capybara <sakibara.ex.9175@gmail.com>
2023-07-29 12:12:38 +0900

    動画後半を見て名前を表示させる記述に変更

--- src/main/java/com/Assignment6/helloworld/HelloWorldController.java
+++ src/main/java/com/Assignment6/helloworld/HelloWorldController.java
@@ -3,17 +3,15 @@ package com.Assignment6.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 
 @RestController
 public class HelloWorldController {
 
-    //    ブラウザ上で[localhost:8080/capybara]と入力→[/capybara] はパス?　@GetMapping()の中の記述と同じ　
-    @GetMapping("/capybara")
-    public String sayhello() {
-        return "Welcome to RaiseTech!";
+    //    ブラウザ上で[localhost:8080/capybara]と入力→[/capybara] はパス?　@GetMapping()の中の記述と同じ
+    //    Rest APIの原則？で返すものは統一する→名前を表示させる場合、パスは
+    @GetMapping("/names")
+    public Name names() {
+        return new Name("Saki", "Fujita");
     }
-
 
 }
--- src/main/java/com/Assignment6/helloworld/Name.java
+++ src/main/java/com/Assignment6/helloworld/Name.java
@@ -1,2 +1,28 @@
-package com.Assignment6.helloworld;public class Name {
+package com.Assignment6.helloworld;
+
+public class Name {
+
+    private String Firstname;
+    private String Lastname;
+
+    public Name(String Firstname, String Lastname) {
+        this.Firstname = Firstname;
+        this.Lastname = Lastname;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
 }
